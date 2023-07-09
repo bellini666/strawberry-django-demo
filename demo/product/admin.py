693ce47c645e0e9ad53e0aa2fb3c447ab970b5e0
Thiago Bellini Ribeiro <thiago@bellini.dev>
2023-07-09 13:23:42 -0300

    feat: implement product app

--- demo/product/admin.py
+++ demo/product/admin.py
@@ -1,3 +1,49 @@
+from __future__ import annotations
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Brand, Product, ProductImage
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "name",
+    ]
+    search_fields = [
+        "id",
+        "name",
+    ]
+    list_display_links = [
+        "id",
+    ]
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    autocomplete_fields = [
+        "product",
+    ]
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "name",
+        "price",
+    ]
+    search_fields = [
+        "id",
+        "name",
+    ]
+    list_display_links = [
+        "id",
+    ]
+    autocomplete_fields = [
+        "brand",
+    ]
+    inlines = [
+        ProductImageInline,
+    ]
--- demo/product/models.py
+++ demo/product/models.py
@@ -1,3 +1,90 @@
-from django.db import models
+from __future__ import annotations
 
-# Create your models here.
+from typing import TYPE_CHECKING
+
+import strawberry
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django_choices_field.fields import TextChoicesField
+
+if TYPE_CHECKING:
+    from django.db.models.manager import RelatedManager
+
+
+class Brand(models.Model):
+    """Product brand model."""
+
+    class Meta:
+        verbose_name = _("Brand")
+        verbose_name_plural = _("Brands")
+
+    name = models.CharField(
+        verbose_name=_("Name"),
+        max_length=255,
+    )
+
+
+class Product(models.Model):
+    """Product model."""
+
+    class Meta:
+        verbose_name = _("Product")
+        verbose_name_plural = _("Products")
+
+    @strawberry.enum(name="ProductKind")
+    class Kind(models.TextChoices):
+        PHYSICAL = "physical", _("Physical")
+        VIRTUAL = "virtual", _("Virtual")
+
+    images: RelatedManager[ProductImage]
+
+    name = models.CharField(
+        verbose_name=_("Name"),
+        max_length=255,
+    )
+    kind = TextChoicesField(
+        verbose_name=_("Category"),
+        choices_enum=Kind,
+        default=Kind.PHYSICAL,
+    )
+    brand_id: int | None
+    brand = models.ForeignKey(
+        Brand,
+        verbose_name=_("Product"),
+        on_delete=models.RESTRICT,
+        related_name="images",
+        db_index=True,
+        null=True,
+        blank=True,
+        default=None,
+    )
+    description = models.TextField(
+        verbose_name=_("Description"),
+        default="",
+    )
+    price = models.DecimalField(
+        verbose_name=_("Price"),
+        max_digits=24,
+        decimal_places=2,
+    )
+
+
+class ProductImage(models.Model):
+    """Product image model."""
+
+    class Meta:
+        verbose_name = _("Image")
+        verbose_name_plural = _("Images")
+
+    product_id: int
+    product = models.ForeignKey(
+        Product,
+        verbose_name=_("Product"),
+        on_delete=models.RESTRICT,
+        related_name="images",
+        db_index=True,
+    )
+    image = models.ImageField(
+        verbose_name=_("Image"),
+        max_length=2000,
+    )
